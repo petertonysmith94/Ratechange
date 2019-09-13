729523477c362dcb6fe94d124f6c37f19ab47d9e
Peter Smith <peter.smith@4cplatform.co.uk>
2019-09-13 10:00:13 +0100

    Implemented the latest exchange rate

--- gql/src/server.js
+++ gql/src/server.js
@@ -1,16 +1,46 @@
 const { ApolloServer } = require('apollo-server');
+const fetch = require('node-fetch');
+const { get } = require('lodash');
 
 // The schema for object type definitions 
 const typeDefs = `
-  type Query {
+  type Rate {
+    currency: String
+    rate: Float
+  }
 
+  type Rates {
+    rates: [Rate]
+    base: String
+    date: String
+  }
+
+  type Query {
+    exchange_latest(base: String): Rates
   }
 `;
 
 // Resolvers are only called when data is requested
 const resolvers = {
   Query: {
+    exchange_latest: (_, params) => {
+      const endpoint = `${ process.env.EXCHANGE_RATE_API }/latest`
 
+      return fetch().then(res => res.json())
+    }
+  },
+  Rates: {
+    rates: (data) => {
+      const rates = get(data, 'rates', {});
+
+      // Tranform rates into an array
+      return Object.keys(rates).map(currency => {
+        return {
+          currency,
+          rate: rates[currency]
+        }
+      });
+    }
   }
 };
 
